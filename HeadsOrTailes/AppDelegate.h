422d81d6d327a42eb9f4d270fe82595280877d09
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-04-07 08:12:00 -0300

    Initial Commit

---
+++ HeadsOrTailes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeadsOrTailes
+//
+//  Created by Michael Maloof on 4/7/16.
+//  Copyright © 2016 Jetpack Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeadsOrTailes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeadsOrTailes
+//
+//  Created by Michael Maloof on 4/7/16.
+//  Copyright © 2016 Jetpack Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
